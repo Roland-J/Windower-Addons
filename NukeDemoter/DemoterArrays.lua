fc184aced78a77de3b0a77c479400e7b371f2b21
Roland-J <107378114+Roland-J@users.noreply.github.com>
2022-06-13 07:30:36 -0500

    Add files via upload

--- NukeDemoter/DemoterArrays.lua
+++ NukeDemoter/DemoterArrays.lua
@@ -1,54 +1,51 @@
 local res = require('resources')
 
--- Array of spell kinds ordered by potency
+-- Array of spell kinds to demote, each ordered by potency
+-- NOTE: Don't demote spells, such as protect or an enfeeble, where there'd be a long-lasting detriment to doing so
 -- CREDIT: Array built loosely on Zarianna's GEO.lua's degrade_array
 kind_arrays = {
 	-- Fire
-	['Fire'] = {'Fire', 'Fire II', 'Fire III', 'Fire IV', 'Fire V'},
+	['Fire'] = {'Fire', 'Fire II', 'Fire III', 'Fire IV', 'Fire V', 'Firaja', 'Fire VI'}, --the -aja gets filtered out
 	['Firaga'] = {'Firaga', 'Firaga II', 'Firaga III'},
 	['Fira'] = {'Fira', 'Fira II'},
 	['Flare'] = {'Flare', 'Flare II'},
-	['FireMixed'] = {'Fire', 'Fire II', 'Fire III', 'Fire IV', 'Fire V', 'Firaja', 'Fire VI'},
 	
 	-- Ice
-	['Blizzard'] = {'Blizzard', 'Blizzard II', 'Blizzard III', 'Blizzard IV', 'Blizzard V'},
+	['Blizzard'] = {'Blizzard', 'Blizzard II', 'Blizzard III', 'Blizzard IV', 'Blizzard V', 'Blizzaja', 'Blizzard VI'}, --the -aja gets filtered out
 	['Blizzaga'] = {'Blizzaga', 'Blizzaga II', 'Blizzaga III'},
 	['Blizzara'] = {'Blizzara', 'Blizzara II'},
 	['Freeze'] = {'Freeze', 'Freeze II'},
-	['BlizzardMixed'] = {'Blizzard', 'Blizzard II', 'Blizzard III', 'Blizzard IV', 'Blizzard V', 'Blizzaja', 'Blizzard VI'},
 	
 	-- Wind
-	['Aero'] = {'Aero', 'Aero II', 'Aero III', 'Aero IV', 'Aero V'},
+	['Aero'] = {'Aero', 'Aero II', 'Aero III', 'Aero IV', 'Aero V', 'Aeroja', 'Aero VI'}, --the -aja gets filtered out
 	['Aeroga'] = {'Aeroga', 'Aeroga II', 'Aeroga III'},
 	['Aerora'] = {'Aerora', 'Aerora II'},
 	['Tornado'] = {'Tornado', 'Tornado II'},
-	['AeroMixed'] = {'Aero', 'Aero II', 'Aero III', 'Aero IV', 'Aero V', 'Aeroja', 'Aero VI'},
 	
 	-- Earth
-	['Stone'] = {'Stone', 'Stone II', 'Stone III', 'Stone IV', 'Stone V'},
+	['Stone'] = {'Stone', 'Stone II', 'Stone III', 'Stone IV', 'Stone V', 'Stoneja', 'Stone VI'}, --the -aja gets filtered out
 	['Stonega'] = {'Stonega', 'Stonega II', 'Stonega III'},
 	['Stonera'] = {'Stonera', 'Stonera II'},
 	['Quake'] = {'Quake', 'Quake II'},
-	['StoneMixed'] = {'Stone', 'Stone II', 'Stone III', 'Stone IV', 'Stone V', 'Stoneja', 'Stone VI'},
 	
 	-- Lightning
-	['Thunder'] = {'Thunder', 'Thunder II', 'Thunder III', 'Thunder IV', 'Thunder V'},
+	['Thunder'] = {'Thunder', 'Thunder II', 'Thunder III', 'Thunder IV', 'Thunder V', 'Thundaja', 'Thunder VI'}, --the -aja gets filtered out
 	['Thundaga'] = {'Thundaga', 'Thundaga II', 'Thundaga III'},
 	['Thundara'] = {'Thundara', 'Thundara II'},
 	['Burst'] = {'Burst', 'Burst II'},
-	['ThunderMixed'] = {'Thunder', 'Thunder II', 'Thunder III', 'Thunder IV', 'Thunder V', 'Thundaja', 'Thunder VI'},
 	
 	-- Water
-	['Water'] = {'Water', 'Water II', 'Water III', 'Water IV', 'Water V'},
+	['Water'] = {'Water', 'Water II', 'Water III', 'Water IV', 'Water V', 'Waterja', 'Water VI'}, --the -aja gets filtered out
 	['Waterga'] = {'Waterga', 'Waterga II', 'Waterga III'},
 	['Watera'] = {'Watera', 'Watera II'},
 	['Flood'] = {'Flood', 'Flood II'},
-	['WaterMixed'] = {'Water', 'Water II', 'Water III', 'Water IV', 'Water V', 'Waterja', 'Water VI'},
 	
 	-- Dark
 	['Aspir'] = {'Aspir', 'Aspir II', 'Aspir III'},
+	['Drain'] = {'Drain', 'Drain II', 'Drain III'},
 	
 	-- Light
+	['Banish'] = {'Banish', 'Banish II', 'Banish III', 'Banish IV'},
 	['Cure'] = {'Cure', 'Cure II', 'Cure III', 'Cure IV', 'Cure V', 'Cure VI'}, --why not?
 	['Curaga'] = {'Curaga', 'Curaga II', 'Curaga III', 'Curaga IV', 'Curaga VI'}, --why not?
 }
